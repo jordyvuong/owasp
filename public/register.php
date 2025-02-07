cb119bf53065a970dab196f077a59df859f23909
Maeva Nono <maeva.nono@efrei.net>
2025-02-07 12:09:59 +0100

    new version

--- public/register.php
+++ public/register.php
@@ -9,18 +9,26 @@ function registerUser($pdo, $name, $password, $confirmPassword, $email) {
         // Vérifie si les mots de passe correspondent
         if ($password !== $confirmPassword) {
             throw new Exception("Les mots de passe ne correspondent pas.");
-        }try{
+        }
+
+        try {
+            // Hashage du mot de passe
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-            $stmp = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
-            $stmp->execute([
+            
+            // Préparation et exécution de la requête
+            $stmt = $pdo->prepare("INSERT INTO users (name, email, password, role_id) VALUES (:name, :email, :password, :2)");
+            $stmt->execute([
                 'name' => $name,
                 'email' => $email,
                 'password' => $hashed_password
             ]);
-            return true;
+
+            return true; // Inscription réussie
         } catch (Exception $e) {
             throw new Exception("Erreur lors de l'inscription : " . $e->getMessage());
         }
+    } else {
+        throw new Exception("Tous les champs sont requis.");
     }
 }
 require_once '../templates/register_form.php';
--- templates/register_form.php
+++ templates/register_form.php
@@ -7,10 +7,9 @@
     <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
-    <div class="login-container">
-        <h2>Connexion</h2>
-        <?php if (!empty($error)) { echo "<p class='error'>$error</p>"; } ?>
-        <form method="POST" action="login.php">
+    <div>
+        <h2>S'inscrire</h2>
+        <form method="POST" action="register.php">
             <label for="name">Nom :</label>
             <input type="text" name="name" id="name">
 
@@ -25,7 +24,7 @@
             
             <button type="submit">Se connecter</button>
         </form>
-        <p>Pas encore de compte ? <a href="register.php">Inscrivez-vous ici</a></p>
+        <p>Vous avez déjà un compte ? <a href="login.php">Se connecter</a></p>
     </div>
 </body>
 </html>
